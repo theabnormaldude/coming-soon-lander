3dd1c194195a46d3e6d46e7c161822ae83972ebe
Shiv Jethi <shivjethi04@gmail.com>
2019-02-22 18:37:42 +0530

    Test Commit

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@
 (function ($) {
     "use strict";
 
-    /*==================================================================
-    [ Validate ]*/
+   
     var input = $('.validate-input .input100');
 
     $('.validate-form').on('submit',function(){
@@ -38,7 +37,52 @@
             }
         }
     }
-
+    class MouseLight {
+        constructor(selector, options) {
+          const defaults = {
+            background: '#000',
+            highlight: '#fff',
+            size: '400px',
+          }
+          this.x = 0;
+          this.y = 0;
+          this.gradientElement = document.querySelector(selector);
+          this.config = Object.assign(defaults, options);
+          this.width = document.documentElement.clientWidth;
+          this.height = document.documentElement.clientHeight;
+          document.addEventListener('mousemove', this.mouseMoveHandler.bind(this));
+          
+          // update on initialisation
+          this.updateGradient();
+          this.animate = this.animate.bind(this);
+          requestAnimationFrame(this.animate);
+        }
+        
+        mouseMoveHandler(event) {
+          const { clientX, clientY } = event;
+          const x = (clientX / this.width);
+          const y = (clientY / this.height);
+          this.x += (x - this.x) * 0.1;
+          this.y += (y - this.y) * 0.1;
+        }
+        
+        animate() {
+          this.updateGradient();
+          requestAnimationFrame(this.animate);
+        }
+        
+        updateGradient() {
+          const {size, highlight, background} = this.config;
+           this.gradientElement.style.background = `radial-gradient(${size} at ${this.x * 100}% ${this.y * 100}%, ${highlight}, ${background})`;
+        }
+      }
+      
+      // Create an instance
+      new MouseLight('.mouse-gradient', {
+        background: '#7117ea',
+        highlight: '#17ead9',
+        size: '1800px',
+      });
     function showValidate(input) {
         var thisAlert = $(input).parent();
 
